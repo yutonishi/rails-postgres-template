4eabb8a346e4a0c2e0e302435bbedfd948b3605b
yutonishi <yuto.nishi10@gmail.com>
2023-05-05 14:18:44 +0900

    Setup SCSS compile

--- app/javascript/application.js
+++
@@ -1,4 +0,0 @@
-// Entry point for the build script in your package.json
-
-import './javascripts/fetchJquery'
-import './javascripts/test'
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 const path    = require("path")
 const webpack = require("webpack")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: [
+      './app/javascript/entry/application.js',
+      './app/javascript/entry/application.scss'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss', '.css'],
   },
   output: {
     filename: "[name].js",
@@ -15,6 +31,8 @@ module.exports = {
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
-    })
+    }),
+    new RemoveEmptyScriptsPlugin(),
+    new MiniCssExtractPlugin(),
   ]
 }
